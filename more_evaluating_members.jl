a73df61762c71f094edd1ba079799ca861ecc9dd
fanyuan-lab <55163503+fanyuan-lab@users.noreply.github.com>
2022-07-01 14:55:26 +0800

    Update more_evaluating_members.jl

--- more_evaluating_members.jl
+++ more_evaluating_members.jl
@@ -2,13 +2,11 @@ using Random, StatsBase, Statistics
 
 Random.seed!(0)
 
-function groupFormation(N::Int, η::Float64)
-    c = 2 #the number of founders
-    n = 2 #the number of admitted members
-    m = c #m evaluating members at each time step
+function groupFormation(N::Int, η::Float64, f::Float64, m::Int)
+    c = m #initial positive founders
+    n = m #number of admitted nodes
     while n < N
         w = (1 - 2η) * c / n + η
-        f = 1 / 2 #f∈[0,1]
         tp = f * w^m / (f * w^m + (1 - f) * (1 - w)^m)
         if tp > rand()
             c += 1
@@ -20,24 +18,11 @@ function groupFormation(N::Int, η::Float64)
     return c / N
 end
 
-function measure(N::Int, η::Float64; iterations=500)
+function calc_cohesion(N::Int, η::Float64, f::Float64, m::Int; iterations=500)
     cohesion_vec = zeros(iterations)
     for iter in 1:iterations
-        cohesion = groupFormation(N, η)
+        cohesion = groupFormation(N, η, f, m)
         cohesion_vec[iter] = cohesion
     end
     return mean(cohesion_vec), std(cohesion_vec)
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
